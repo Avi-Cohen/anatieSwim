fcb86356ff4828abe1705af16237e8356ef678db
Avi Cohen <aviko.cohen@gmail.com>
2020-06-28 10:34:16 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import data from './data';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import './App.css';
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
+
+function App() {
+
+  const openMenu = () => {
+    document.querySelector('.sidebar').classList.add('open');
+  };
+  const closeMenu = () => {
+    document.querySelector('.sidebar').classList.remove('open');
+  };
+  return (
+    <Router>
+      <div classNameName="grid-container">
+          <header className="header">
+              <div className="brand">
+                  <button onClick={openMenu}>
+                    &#9776;
+                  </button>
+                  <Link to="/">anatieSwim</Link>
+              </div>
+              <div className="header-links">
+                  <a href="cart.html">Cart </a>
+                  <a href="signin.html">Sign In</a>
+              </div>
+          </header>
+          <aside className="sidebar">
+              <h3>Shopping Categories</h3>
+              <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+              <ul className="categories">
+                  <li>
+                      <a href="index.html">Print </a>
+                  </li>
+                  <br></br>
+                  <li>
+                    <a href="index.html">Solid </a> 
+                  </li>
+                  <br></br>
+              </ul>
+          </aside>
+          <main className="main">
+              <div className="content">
+                <Route path="/product/:id" component={ProductScreen}/>
+                <Route path="/" exact={true} component={HomeScreen}/>
+              </div>
+          </main>
+          <footer className="footer">
+              All right reserved to Avi Cohen.
+          </footer>
+      </div>
+    </Router>    
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,32 @@
+export default {
+    products:[
+        {
+        _id: '001',
+        name: 'Dots B',
+        category: 'Print',
+        image: '/images/Dots.jpg',
+        price: 85,
+        },
+        {
+        _id: '002',
+        name: 'Pattern B',
+        category: 'Print',
+        image: '/images/Pattern.jpg',
+        price: 100,
+        },
+        {
+        _id: '002',
+        name: 'Pattern B',
+        category: 'Print',
+        image: '/images/Pattern.jpg',
+        price: 100,
+        },
+        {
+        _id: '001',
+        name: 'Dots B',
+        category: 'Print',        
+        image: '/images/Dots.jpg',
+        price: 85,
+        }
+    ]
+}
